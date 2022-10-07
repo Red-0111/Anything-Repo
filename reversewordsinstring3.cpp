b71806241a26c2b34ee3ef14155ee34fa8b52ab2
abhay <abhay.chauhan444@gmail.com>
2022-10-07 21:26:31 +0530

    added solution of reverse words in string3

---
+++ reversewordsinstring3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public: 
+    string reverseWords(string s) {
+        int length = s.length();
+        int i = 0;
+        while (i < length) {
+            int start = i;
+            while (i < length && s[i] != ' ') {
+                i++;
+            }
+            int left = start, right = i - 1;
+            while (left < right) {
+                swap(s[left], s[right]);
+                left++;
+                right--;
+            }
+            while (i < length && s[i] == ' ') {
+                i++;
+            }
+        }
+        return s;
+    }
+};
