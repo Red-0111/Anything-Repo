11529530d291268ed7bf32418322a2f6126062a2
Heckerdm <91755402+Heckerdm@users.noreply.github.com>
2022-10-11 08:59:23 +0530

    Create Pacman.py
    Pacman

---
+++ Pacman.py
@@ -0,0 +1,31 @@
+
+The Code Given Below Is For The Python File Enemies
+import pygame
+import random
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 576
+
+# Define some colors
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+BLUE = (0,0,255)
+GREEN = (0,255,0)
+RED = (255,0,0)
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self,x,y,color,width,height):
+        # Call the parent class (Sprite) constructor
+        pygame.sprite.Sprite.__init__(self)
+        # Set the background color and set it to be transparent
+        self.image = pygame.Surface([width,height])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+
+
+class Ellipse(pygame.sprite.Sprite):
+    def __init__(self,x,y,color,width,height):
+        # Call the parent class (Sprite) constructor
+        pygame.sprite.Sprite.__init__(self)
+        # Set the background 
