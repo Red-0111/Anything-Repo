a32433ce0594c3923bcde7b0d7b1aee838191946
CoolCoder3955 <115133249+CoolCoder3955@users.noreply.github.com>
2022-10-15 15:59:40 +0530

    Create FIBONACCI.py

---
+++ FIBONACCI.py
@@ -0,0 +1,12 @@
+n = int(input())
+a = 0
+b = 1
+sum = 0
+count = 1
+print("Fibonacci series is: ", end = " ")
+while(count <= n):
+  count += 1
+  print(a, end=" ")
+  a = b
+  b = sum
+  sum = a + b
